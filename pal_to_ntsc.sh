6ba1d65ea386bddcc630f53539ad0dad4b2e892a
hajdutialb <71173827+hajdutialb@users.noreply.github.com>
2021-06-16 20:18:12 +0200

    Update pal_to_ntsc.sh

--- pal_to_ntsc.sh
+++ pal_to_ntsc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # slow down video from PAL to NTSC speed without changing audio pitch. Audio quality is preserved (lossless flac), video quality is not (lossy h264)
+
+# e.g. ./pal_to_ntsc.sh INPUT.mp4 OUTPUT.mkv
 ffmpeg -i "$1" -filter:v "setpts=25025/24000*PTS" -map 0:v -map 0:a -vcodec h264 -preset ultrafast -acodec flac -filter:a atempo=24000/25025 -r 24000/1001 "$2"
